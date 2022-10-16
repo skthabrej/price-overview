294ebce11fcb2dd898414cd35f157c60c07371cf
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-16 17:22:04 +0530

    price-details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import ReviewsCarousel from './components/ReviewsCarousel'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const reviewsData = [
+  {
+    imgUrl: 'https://i.ibb.co/tzmx8NT/vksd-vsv.webp',
+    Price: '1899/-',
+    Brand: 'Levi`s',
+    description:
+      'trusted for innovation, quality, performance, durability, style, fit and more.',
+  },
+  {
+    imgUrl: 'https://i.ibb.co/kgRDGhp/upload9223368955665729970.jpg',
+    Price: '1999/-',
+    Brand: 'Superdry',
+    description:
+      'world leading hand-drawn graphics and tailored fits with diverse styling.',
+  },
+  {
+    imgUrl: 'https://i.ibb.co/LxrQS8J/0985fc7313b663eeb19ed9354475a015.jpg',
+    Price: '1989/-',
+    Brand: 'French Connection',
+    description:
+      'Tough, gritty,high quality/stylish products, and realistic',
+  },
+  {
+    imgUrl: 'https://i.ibb.co/LND5QHr/nike-m-nk-df-tee-jdi.webp',
+    Price: '2599/-',
+    Brand: 'Nike',
+    description:
+      'nnovative technology, high quality/stylish products,regionally involved, and globally responsible.',
+  },
+]
 
-export default App;
+const App = () => <ReviewsCarousel reviewsData={reviewsData} />
+
+export default App
---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,53 @@
+import './index.css'
+import {Component} from 'react'
+
+class ReviewsCarousel extends Component {
+  state = {count: 0}
+
+  onIncrement = () => {
+    const {count} = this.state 
+    if (count < 3) {
+      this.setState(prevState => ({count: prevState.count + 1}))
+    }
+  }
+
+  onDecrement = () => {
+    const {count} = this.state 
+    if (count >= 1) {
+      this.setState(prevState => ({count: prevState.count - 1}))
+    }
+  }
+
+  render() {
+    const {count} = this.state
+    const {reviewsData} = this.props 
+    const {imgUrl, Brand, description, Price} = reviewsData[count]
+    return (
+      <div className="bg-container">
+        <div className='card'>
+          <h1 className="heading-1">B r a n d s</h1>
+          <div className="img-person">
+            <img className="img-style" src={imgUrl} alt={Price}/>
+          </div>
+          <div className="img-person">
+            <div className="arrows-content-container">
+              <button type="button" className="btn-style" onClick={this.onDecrement} testid="leftArrow">
+                <img className="img-arrows" src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png" alt="left-arrow"/>
+              </button>
+              <p className="price-name">{Price}</p>
+              <button type="button" className="btn-style" onClick={this.onIncrement} testid="rightArrow">
+                <img className="img-arrows" src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png" alt="right-arrow"/>
+              </button>
+            </div>
+          </div>
+          <div>
+            <p className="paragraph">{Brand}</p>
+            <p className="paragraph-1">{description}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
